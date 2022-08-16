88e8dee4e6f1d8a47d9e65bd8b273af1bc311a97
xiaheshun <755320689@qq.com>
2022-08-16 10:43:13 +0800

    初始化

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    base: '/proxx-blog/',
+    lang: 'zh-CN',
+    title: 'Proxx博客',
+    description: '第一个 VuePress 站点',
+})
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Hello Blog
+
+Author: XiaHeShun
