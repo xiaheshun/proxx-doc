772058e6611c5246b5829d96869518f5153f3964
xiaheshun <755320689@qq.com>
2022-08-16 11:24:22 +0800

    删除前缀

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-    base: '/proxx-doc/',
+    base: '/',
     lang: 'zh-CN',
     title: 'Proxx博客',
     description: '第一个 VuePress 站点',
