c1bad1756500ccf8ee65a1ae128e9dfdef5c952c
夏和顺 <xiaheshun@foxmail.com>
2022-08-18 23:01:04 +0800

    参考官方文档，形成目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,22 +15,37 @@ module.exports = {
         logo: 'https://proxx.oss-cn-beijing.aliyuncs.com/logo.svg',
         logoDark: 'https://proxx.oss-cn-beijing.aliyuncs.com/logo_dark.svg',
         // 仓库
-        repo: 'https://github.com/xiaheshun?tab=repositories&q=proxx&type=&language=&sort=name',
+        repo: 'https://github.com/xiaheshun/',
         // 默认主题配置
         navbar: [
             {text: '主页', link: '/',},
-            {text: '博客', link: '/vuepress/构建静态博客问题.html',},
+            {text: '指南', link: '/guide/',},
+            {text: '建站', link: '/blog/',},
+            {text: '码云', link: 'https://gitee.com/xiaheshun',},
         ],
-        sidebar: {
-            '/vuepress/':[
+        sidebar:{
+            '/guide/':[
                 {
-                    title: '博客',
-                    collapsable: false,
+                    text: '知识库指南',
+                    path: '/guide/'
+                }
+            ],
+            '/blog/':[
+                {
+                    text: '1.初始准备工作',
+                    collapsible: false,
                     children: [
-                        { title: '构建静态博客问题', path: '/vuepress/构建静态博客问题.html' },
+                        '1.1环境安装.md'
+                    ]
+                },
+                {
+                    text: '问题汇总',
+                    collapsible: true,
+                    children: [
+                        'Q1GitHub流水线构建问题.md'
                     ]
                 }
-            ]
+            ],
         }
     }),
     plugins: [
